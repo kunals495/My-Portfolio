9e385ee11361a731fc7063e70b2951f90db9fcff
Kunal Singh <kunalsingj7@gmail.com>
2025-07-23 19:26:16 +0530

    Change in Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,5 @@
 import Lottie from "lottie-react";
-import education from "../assets/lottie/education.json";
+import education from "../assets/lottie/Education.json";
 import IIIT_KOTA from "../assets/mgm logo.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
